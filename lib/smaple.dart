4922dae25fd5142b4f5de7ac6ae9de41c7e0eee4
AYSHATHFAHMIDA <ayshafahmi26@gmail.com>
2022-09-19 16:00:07 +0530

    first commit

---
+++ lib/smaple.dart
@@ -0,0 +1,18 @@
+//main function   void-return type main-function name (build in)  ()-function representation
+import 'dart:io';
+
+void main(){
+  //variable declaration
+  //data types-String,int,bool,double
+  //syntax=> datatype variablename=value;
+  String name='ayshath fahmida';
+  int age=22;
+  double mark=9.12;
+  String email='ayshafahmi26@gmail.com';
+  // int phone=9947285100;
+  //String interpoation
+  print('my name is $name');
+  print('i am $age yrs old');
+  stdout.write("my cgpa is $mark");
+  stdout.write("my email id is $email");
+}
